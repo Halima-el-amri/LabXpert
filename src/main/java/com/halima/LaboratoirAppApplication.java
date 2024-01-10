a403b7de53c776148b02bc0f4a23e7bed0cce3ee
EL AMRI HALIMA <elamrihalima1@gmail.com>
2024-01-10 16:00:39 +0100

    commit for new changes

---
+++ src/main/java/com/halima/LaboratoirAppApplication.java
@@ -0,0 +1,13 @@
+package com.halima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoirAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoirAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/halima/LaboratoirAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoirAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
